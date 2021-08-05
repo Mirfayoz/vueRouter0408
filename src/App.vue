efca081565bbe8a71bfcefc0e70714a24fda0d26
Mirfayoz <mirfayozmirzamuradov600@gmail.com>
2021-08-05 04:19:04 -0700

    start project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="menu">
+      <router-link v-for='(menu,index) of menus' :key='index' :to='menu.link' exact active-class="active">
+        {{menu.title}}
+      </router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      menus:[
+        {title: 'Home', link: '/'},
+        {title: 'Category', link: '/category'},
+        {title: 'Devices', link: '/devices'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .menu{
+    display: flex;
+    justify-content: center;
+  }
+  .menu a{
+    margin: 0px 10px;
+    text-decoration: none;
+    color: #000;
+  }
+  .menu a.active{
+    text-decoration: underline;
+  }
+</style>
---
+++ src/components/Device.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>Device: {{id}}</h3>
+        <h3>Query: {{this.$route.query.title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id: this.$router.currentRoute.params['id']
+        }
+    },
+    watch:{
+        $router(to,from){
+            console.log(to,from)
+            this.id = to.params['id']
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
